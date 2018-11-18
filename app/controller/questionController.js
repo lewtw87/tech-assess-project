f21a40d3d37a92033356eb2c89ba3d59460414b0
Alvin Lew <lewtw87@gmail.com>
2018-11-18 22:16:07 +0800

    checkin

--- app/controller/questionController.js
+++ app/controller/questionController.js
@@ -3,10 +3,10 @@ const db = require('../../db.js');
 //Gov Tech Endpoint 1
 exports.create_question = function (req, res) {
 
-    //Check if all tags are available
-    //let tagsArray = JSON.parse(req.body.tags);
+    //Create tag
     const tags = req.body.tags.map(tagItem => db.Tags.findOrCreate({ where: { tag: tagItem }, defaults: { tag: tagItem }}).spread((tag, created) => tag));
 
+    //Create question
     db.Questions.create(req.body)
     .then(questions => Promise.all(tags).then(storedTags => questions.addTags(storedTags)).then(() => questions))
     .then(questionTag => res.status(201).json({id: questionTag.id })) //return 201 for POST succeed status
@@ -20,24 +20,6 @@ exports.create_question = function (req, res) {
 
 //Gov Tech Endpoint 2
 exports.get_question_by_tag = function (req, res) {
-
-    /*db.Questions.findAll({
-        attributes: ['id', 'question'],
-        include: [
-            { 
-                model: db.Tags, 
-                where: { tag: req.query.tag },
-                attributes: []
-            }
-        ],
-        group: ['question.id'],
-        having: [db.Sequelize.literal('COUNT(DISTINCT ?) >= ?'), '`tag.tag`', req.query.tag.length]
-    })
-    .then(questions => res.json(questions))
-    .catch(function(err) {
-        console.log(err);
-        res.status(400).json({ error: true, message: "There is a problem querying the database."});
-    });*/
     
     let tagArrayString = "";
     let tagCount = 0;
@@ -64,6 +46,24 @@ exports.get_question_by_tag = function (req, res) {
         res.status(400).json({ error: true, message: "There is a problem querying the database."});
     });
 
+    /*db.Questions.findAll({
+        attributes: ['id', 'question'],
+        include: [
+            { 
+                model: db.Tags, 
+                where: { tag: req.query.tag },
+                attributes: []
+            }
+        ],
+        group: ['question.id'],
+        having: [db.Sequelize.literal('COUNT(DISTINCT ?) >= ?'), '`tag.tag`', req.query.tag.length]
+    })
+    .then(questions => res.json(questions))
+    .catch(function(err) {
+        console.log(err);
+        res.status(400).json({ error: true, message: "There is a problem querying the database."});
+    });*/
+
 };
 
 //Gov Tech Endpoint 3
@@ -75,24 +75,27 @@ exports.create_quiz = function (req, res) {
     } 
     else
     {
+        //get summation of weighting
         let sumOfWeights = req.body.questions.reduce(function(memo, question) {
             return memo + question.weight;
             }, 0);
         let selectedWeigths = {};
         let selectedId = [];
         const probabilityOfDistribution = sumOfWeights * 20; //Alvin: Higher for more accuracy and less variation
-
+        
+        //assign dictionary for each question id, init it with 0
         for (var key in req.body.questions) {
             const question = req.body.questions[key];
             selectedWeigths[question.id] = 0;
         }
 
+        //from 0 to total distribution, randomly distribute based on weight
         for (var i = 0; i < probabilityOfDistribution; i++) {
-            let random = Math.random() * (sumOfWeights + 1);
+            let random = Math.random() * (sumOfWeights); //generate random number from 0 to total
             for (var key in req.body.questions) {
                 const question = req.body.questions[key];
                 //console.log(question);
-                random -= question.weight;
+                random -= question.weight; //if random - weight <= 0 then distribute it, item count + 1
                 if(random <= 0)
                 {
                     selectedWeigths[question.id] = (selectedWeigths[question.id] || 0) + 1;
@@ -101,19 +104,19 @@ exports.create_quiz = function (req, res) {
             }
         }  
 
+        //get the distributed amount with the question id
         var sortedWeights = Object.keys(selectedWeigths).map(function(key) {
             return [key, selectedWeigths[key]];
         });
-        
-        
-        // Sort the array based on the second element
+        // Sort the array base on distributed amount
         sortedWeights.sort(function(first, second) {
             return second[1] - first[1];
         });
 
         console.log(sortedWeights); // ALV: first is id, 2nd is distributed probability (% of occurence)
 
-        for (var i = 0; i < req.body.length; i++) { // now lets iterate in sort order
+        //get the question id in order (within the parameter length)
+        for (var i = 0; i < req.body.length; i++) { 
             var key = parseInt(sortedWeights[i][0]);
             selectedId.push(key);
         } 
@@ -125,40 +128,3 @@ exports.create_quiz = function (req, res) {
 };
 
 
-
-/*
-exports.list_questions = function (req, res) {
-    Question.getAllQuestions(function (err, question) {
-        if (err)
-            res.send(err);
-
-        res.send(question);
-    });
-};
-
-exports.get_question_by_id = function (req, res) {
-    Question.getQuestionById(req.params.questionId, function (err, question) {
-        if (err)
-            res.send(err);
-        res.json(question);
-    });
-};
-
-
-
-
-exports.update_question = function (req, res) {
-    Question.updateById(req.params.questionId, new Question(req.body), function (err, question) {
-        if (err)
-            res.send(err);
-        res.json(question);
-    });
-};
-
-exports.delete_question = function (req, res) {
-    Question.remove(req.params.questionId, function (err, question) {
-        if (err)
-            res.send(err);
-        res.json({ message: 'Question successfully deleted' });
-    });
-};*/
