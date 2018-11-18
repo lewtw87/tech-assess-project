65fdf179692a555fd6d1fa55155dcf9adfe5d1db
Alvin Lew <lewtw87@gmail.com>
2018-11-18 14:20:58 +0800

    checkin

--- test/mochaTest.js
+++ test/mochaTest.js
@@ -54,37 +54,104 @@ describe('Users', function() {
         });
     });
 });
-
-let questionDetails = {
-    'question': 'What is the boiling point of water?',
-    'tags': ['science', 'physics', 'easy']
-}
     
 describe('Questions', () => {
+
     before(function(done) {
-        db.Questions.destroy({where: { question: 'What is the boiling point of water?'}})
+        db.Questions.destroy({where: 
+            { question: ['What percentage of the earth is covered by water?', 'Why is the sky blue?']}
+            })
             .then(function () {
                 done(null);
             });
     });
-        
-    describe('/api/questions', () => {
-        it('it should create question', (done) => {
+    
+    //End point 1
+    describe('POST Question /api/questions', () => {
+        it('it should create a question', (done) => {
             chai.request(app)
             .post('/api/questions')
             .set('x-access-token', accessToken)
-            .send(questionDetails)
+            .send({
+                'question': 'What percentage of the earth is covered by water?',
+                'tags': ['science', 'physics', 'easy']
+            })
             .end((err, res) => {
                 res.should.have.status(201);
-                res.body.should.be.a('array');
                 res.body.should.have.property('id'); 
                 done();
             });
         });
 
-        it('it should GET all the questions matching all the tags', (done) => {
+        it('it should create a question with previously created tags', (done) => {
             chai.request(app)
-            .get('/api/questions?tag=easy')
+            .post('/api/questions')
+            .set('x-access-token', accessToken)
+            .send({
+                'question': 'Why is the sky blue?',
+                'tags': ['science', 'physics', 'hard']
+            })
+            .end((err, res) => {
+                res.should.have.status(201);
+                res.body.should.have.property('id'); 
+                done();
+            });
+        });
+
+        it('it should not create a question without question', (done) => {
+            chai.request(app)
+            .post('/api/questions')
+            .set('x-access-token', accessToken)
+            .send({
+                'tags': ['science', 'physics', 'easy']
+            })
+            .end((err, res) => {
+                res.should.have.status(400); 
+                done();
+            });
+        });
+
+        it('it should not create a question without tags', (done) => {
+            chai.request(app)
+            .post('/api/questions')
+            .set('x-access-token', accessToken)
+            .send({
+                'question': 'What is the boiling point of water?'
+            })
+            .end((err, res) => {
+                res.should.have.status(400); 
+                done();
+            });
+        });
+    });
+
+    //End point 2
+    describe('GET Question /api/questions', () => {
+        it('it should return error without tag parameter', (done) => {
+            chai.request(app)
+            .get('/api/questions')
+            .set('x-access-token', accessToken)
+            .end((err, res) => {
+                res.should.have.status(400); 
+                done();
+            });
+        });
+
+        /*it('it should not get question matching one of the tags', (done) => {
+            chai.request(app)
+            .get('/api/questions?tag=easy&tag=math')
+            .set('x-access-token', accessToken)
+            .end((err, res) => {
+                res.should.have.status(200);
+                res.body.should.have.property('questions'); 
+                res.body.questions.length.should.be.eql(0);
+                done();
+            });
+        });*/
+
+        it('it should get all the questions matching all the tags', (done) => {
+            chai.request(app)
+            .get('/api/questions?tag=easy&tag=science&tag=physics')
             .set('x-access-token', accessToken)
             .end((err, res) => {
                 res.should.have.status(200);
@@ -94,6 +161,83 @@ describe('Questions', () => {
         });
 
         
+    });
+
+    //End point 3
+    describe('POST Quiz /api/quiz', () => {
+        it('it should return error without questions parameter.', (done) => {
+            chai.request(app)
+                .post('/api/quiz')
+                .set('x-access-token', accessToken)
+                .send({
+                    "length": 3
+                })
+                .end((err, res) => {
+                    res.should.have.status(400); 
+                    done();
+                });
+        });
+
+        it('it should return error without length parameter.', (done) => {
+            chai.request(app)
+                .post('/api/quiz')
+                .set('x-access-token', accessToken)
+                .send({
+                    "questions": [
+                        {"id": 1, "weight": 1},
+                        {"id": 2, "weight": 1.5},
+                        {"id": 3, "weight": 1},
+                        {"id": 4, "weight": 1},
+                        {"id": 5, "weight": 2}
+                    ]
+                })
+                .end((err, res) => {
+                    res.should.have.status(400); 
+                    done();
+                });
+        });
+
+        it('it should return error if length parameter bigger than length of questions parameter.', (done) => {
+            chai.request(app)
+                .post('/api/quiz')
+                .set('x-access-token', accessToken)
+                .send({
+                    "questions": [
+                        {"id": 1, "weight": 1},
+                        {"id": 2, "weight": 1.5},
+                        {"id": 3, "weight": 1},
+                        {"id": 4, "weight": 1},
+                        {"id": 5, "weight": 2}
+                    ],
+                    "length": 6
+                })
+                .end((err, res) => {
+                    res.should.have.status(400); 
+                    done();
+                });
+        });
+
+        it('it should get random quiz result matching the length of the parameter.', (done) => {
+            chai.request(app)
+                .post('/api/quiz')
+                .set('x-access-token', accessToken)
+                .send({
+                    "questions": [
+                        {"id": 1, "weight": 1},
+                        {"id": 2, "weight": 1.5},
+                        {"id": 3, "weight": 1},
+                        {"id": 4, "weight": 1},
+                        {"id": 5, "weight": 2}
+                    ],
+                    "length": 3
+                })
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.have.property('questions'); 
+                    res.body.questions.length.should.be.eq(3);
+                    done();
+                });
+        });
     });
 
 
